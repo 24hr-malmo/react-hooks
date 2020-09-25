599034ddfa29f8b1ef33d1a6302cfa9e9044a2c6
Marcus Thelin <marcus.thelin@24hr.se>
2020-09-25 15:57:47 +0200

    Polyfills and more

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,11 @@
-export { default as useBeforeMount } from './hooks/use-before-mount'
+import 'core-js/es/array';
+import 'core-js/es/object/entries';
+import 'core-js/features/promise';
+import 'core-js/features/object/assign';
+import 'core-js/features/string/repeat';
+import 'core-js/features/string/includes';
+import 'core-js/features/map/includes';
+import 'core-js/features/dom-collections/for-each';
+import 'regenerator-runtime/runtime';
+
+export { default as useBeforeMount } from './hooks/use-before-mount';
